9c2cfd166590a291737235bc65cb6cc01424f619
lmProgramming <mikikubs@gmail.com>
2024-10-09 19:22:44 +0200

    starting lab 3

---
+++ Arduino/lab03/kod02/kod02.ino
@@ -0,0 +1,81 @@
+#define LED_RED 6
+#define LED_GREEN 5
+#define LED_BLUE 3
+
+#define RED_BUTTON 2
+#define GREEN_BUTTON 4
+
+int led[] = {LED_RED, LED_GREEN, LED_BLUE};
+
+void initRGB()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    pinMode(LED_RED, OUTPUT);
+    digitalWrite(LED_RED, HIGH);
+
+    pinMode(LED_GREEN, OUTPUT);
+    digitalWrite(LED_GREEN, LOW);
+
+    pinMode(LED_BLUE, OUTPUT);
+    digitalWrite(LED_BLUE, LOW);
+}
+
+void initButtons()
+{
+    pinMode(RED_BUTTON, INPUT_PULLUP);
+    pinMode(GREEN_BUTTON, INPUT_PULLUP);
+}
+
+bool isGreenButtonPressed()
+{
+    static int debounced_button_state = HIGH;
+    static int previous_reading = HIGH;
+    static int blocker = 0;
+    bool isPressed = false;
+
+    int current_reading = digitalRead(GREEN_BUTTON);
+
+    if (previous_reading != current_reading)
+    {
+        blocker = 1000;
+    }
+
+    if (blocker == 0)
+    {
+        if(current_reading != debounced_button_state)
+        {
+            if (debounced_button_state == HIGH && current_reading == LOW)
+            {
+                isPressed = true;
+            }
+            debounced_button_state = current_reading;
+        }
+    }
+    else
+    {
+        --blocker;
+    }
+
+    previous_reading = current_reading;
+
+    return isPressed;
+}
+
+void setup()
+{
+    initRGB();
+    initButtons();
+}
+
+int led_index = 0;
+void loop()
+{
+    if (isGreenButtonPressed())
+    {
+        digitalWrite(led[led_index], LOW);
+        led_index = ++led_index % 3;
+        digitalWrite(led[led_index], HIGH);
+    }
+}
