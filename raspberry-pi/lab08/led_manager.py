4087fb8dd965a35c80d18e2b6e03a86cca8dd670
mikolaj.kubs <mikikubs@gmail.com>
2024-11-17 13:10:56 +0100

    feat: lab08 might work (no way to test)

--- raspberry-pi/lab08/led_manager.py
+++ raspberry-pi/lab08/led_manager.py
@@ -1,59 +1,64 @@
+from config import *
 import RPi.GPIO as GPIO
 import time
 
 '''Wykorzystując treść opisaną w lab09.pdf proszę o wykonanie zadania:
 
-Napisz program, który pozwala regulować przy pomocy enkodera jasność świecenia diody LED 1/2/3/4 
-w module niebieskich diod świecących. Do obsługi enkodera wykorzystaj zdarzenia (events). W 
-danym czasie świeci się tylko jedna dioda LED. Wybór diody z modułu odbywa się poprzez przyciski. 
+Napisz program, który pozwala regulować przy pomocy enkodera jasność świecenia diody LED 1/2/3/4
+w module niebieskich diod świecących. Do obsługi enkodera wykorzystaj zdarzenia (events). W
+danym czasie świeci się tylko jedna dioda LED. Wybór diody z modułu odbywa się poprzez przyciski.
 Enkoder zmienia jedynie intensywność świecenia. Proszę na konsoli wyświetlać aktualną wartość intensywności.
 '''
 
-LED_PINS: list[int] = [17, 18, 27, 22]
-BUTTON_PINS: list[int] = [5, 6, 13, 19]
-ENCODER_PIN_A = 23
-ENCODER_PIN_B = 24
+intensity: int = 0
+leds: list = [led1, led2, led3, led4]
+diodes: list[GPIO.PWM] = [map(lambda x: GPIO.PWM(x, 50), leds)]
 
-GPIO.setmode(GPIO.BCM)
-for pin in LED_PINS + BUTTON_PINS + [ENCODER_PIN_A, ENCODER_PIN_B]:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-led_pwms: list[PWM] = [GPIO.PWM(pin, 1000) for pin in LED_PINS]
-for pwm in led_pwms:
-    pwm.start(0)
-
-current_led = 0
-brightness = 0
+current_led: int = 0
 
 
-def button_callback(channel):
-    global current_led
-    current_led = BUTTON_PINS.index(channel)
-    print(f"Selected LED: {current_led + 1}")
+def encoder_callback(channel, change):
+    global intensity
 
-
-def encoder_callback(channel):
-    global brightness
-    if GPIO.input(ENCODER_PIN_A) == GPIO.input(ENCODER_PIN_B):
-        brightness = min(brightness + 10, 100)
+    if change > 0:
+        intensity = min(100, intensity + change)
     else:
-        brightness = max(brightness - 10, 0)
-    led_pwms[current_led].ChangeDutyCycle(brightness)
-    print(f"LED {current_led + 1} brightness: {brightness}")
+        intensity = max(0, intensity - change)
+
+    diodes[current_led].ChangeDutyCycle(intensity)
 
 
-for pin in BUTTON_PINS:
-    GPIO.add_event_detect(
-        pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
+def change_led(channel, change):
+    global current_led
+    GPIO.output(leds[current_led], False)
+    current_led = (current_led + change) % 4
+    GPIO.output(leds[current_led], True)
 
-GPIO.add_event_detect(ENCODER_PIN_A, GPIO.BOTH, callback=encoder_callback)
 
-try:
+def setup():
+    diode1 = GPIO.PWM(led1, 50)
+
+    dutyCycle = 0
+    diode1.start(dutyCycle)
+
+    GPIO.add_event_detect(encoderLeft, GPIO.FALLING,
+                          callback=encoder_callback, bouncetime=200)
+
+    GPIO.add_event_detect(encoderRight, GPIO.FALLING,
+                          callback=encoder_callback, bouncetime=200)
+
+
+def loop():
+    print(intensity)
+
+
+def main():
+    setup()
+
     while True:
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    pass
-finally:
-    for pwm in led_pwms:
-        pwm.stop()
+        loop()
+
+
+if __name__ == "__main__":
+    main()
     GPIO.cleanup()
